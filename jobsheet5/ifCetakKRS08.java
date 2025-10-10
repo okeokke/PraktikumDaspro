fa65d542287b52f18355df8a1fa886d2b7c9c93d
okeokke <okeokke77@gmail.com>
2025-10-10 12:23:40 +0700

    Menambah program java ifCetakKRS08 untuk Percobaan 1 Jobsheet 5

---
+++ jobsheet5/ifCetakKRS08.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ifCetakKRS08 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("--- Cetak KRS Siakad ---");
+    System.out.print("Apakah UKT sudah lunas? (true/false): ");
+    boolean uktLunas = sc.nextBoolean();
+    if (uktLunas) {
+      System.out.println("Pembayaran UKT terverifikasi");
+      System.out.println("Silakan cetak KRS dan minta tanda tangan DPA");
+    }
+  }
+}
